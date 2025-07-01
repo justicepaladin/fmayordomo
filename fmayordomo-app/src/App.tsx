07c338a58efd4fc0091197636e4f2e0bbf23598d
Facundo Martín Mayordomo <mayor.facu.m@gmail.com>
2025-07-01 04:42:07 -0300

    first commit
    npm create vite@latest
    react
    typescript
    bc yes

---
+++ fmayordomo-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <img src="/src/assets/avatar.jpg" alt="Avatar" className="avatar" />
+      <h1 className="title">Website under construction</h1>
+      <p className="subtitle">You just lost The Game</p>
+    </div>
+  );
+}
+
+export default App;
