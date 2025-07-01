fce35bdfc2e821cf5aad55e0aadec7df760d6093
Facundo Martín Mayordomo <mayor.facu.m@gmail.com>
2025-07-01 04:54:21 -0300

    Update App.tsx

--- fmayordomo-app/src/App.tsx
+++ fmayordomo-app/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import avatarImage from './assets/avatar.jpg'; // 1. Importa la imagen
 
 function App() {
   return (
     <div className="container">
-      <img src="/src/assets/avatar.jpg" alt="Avatar" className="avatar" />
+      {/* 2. Usa la variable importada en el atributo src */}
+      <img src={avatarImage} alt="Avatar" className="avatar" />
       <h1 className="title">Website under construction</h1>
       <p className="subtitle">You just lost The Game</p>
     </div>
